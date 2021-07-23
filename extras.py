58215fa69926d8252f26db392b9b41c395934eb5
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2021-07-23 16:16:30 +0500

    Add files via upload

---
+++ extras.py
@@ -0,0 +1,166 @@
+from copy import deepcopy
+
+# This configuration was taken from the page we used to learn and practice chess.
+# It shows the improved evaluations per position of a piece
+
+# Link to the website is = https://www.freecodecamp.org/news/simple-chess-ai-step-by-step-1d55a9266977/
+
+piece_square_tables = {
+    "king": [
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+        [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+        [2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0],
+        [2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0]
+    ],
+
+    "queen": [
+        [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+        [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+        [-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+        [-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+        [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+        [-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+        [-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0],
+        [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+    ],
+
+    "rook": [
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5],
+        [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+        [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+        [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+        [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+        [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+        [0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0]
+    ],
+
+    "bishop": [
+        [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+        [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+        [-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0],
+        [-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0],
+        [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0],
+        [-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0],
+        [-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0],
+        [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+    ],
+
+    "knight": [
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+        [-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0],
+        [-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0],
+        [-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0],
+        [-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0],
+        [-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0],
+        [-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0],
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+    ],
+
+    "pawn": [
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+        [1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0],
+        [0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5],
+        [0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+        [0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5],
+        [0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    ],
+
+}
+
+
+# invert a table by reversing then negating
+def get_opposite_table(table):
+    table = deepcopy(table)
+    table.reverse()
+
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            table[i][j] = 0 - table[i][j]
+
+    return table
+
+
+# get the value of a piece
+def get_piece_value(_piece):
+    if _piece == ' ':
+        return 0
+
+    return _piece.points
+
+
+# get the evaluations for a piece at a position according to our piece square tables
+def get_piece_evaluation(_piece):
+    if _piece == ' ':
+        return 0
+
+    row = _piece.row
+    col = _piece.col
+
+    piece_table = piece_square_tables[_piece.name]
+
+    if _piece.team == 'white':
+        return piece_table[row][col]
+
+    # for black, the evaluations have to be reversed (board reversed)
+    else:
+        piece_table = get_opposite_table(piece_table)
+        return piece_table[row][col]
+
+
+# get the total value of a board
+def get_board_evaluation(_board):
+    val = 0
+
+    for row in _board.board:
+        for _piece in row:
+            val += get_piece_value(_piece) + get_piece_evaluation(_piece)
+
+    return val
+
+
+# takes in string input and returns appropriate indexes
+def parse_input(move: str) -> (int, int, int, int):
+    if len(move) != 4:
+        return None, None, None, None
+
+    move_piece = move[0] + move[1]
+    move_to = move[2] + move[3]
+
+    curr_row = int(move_piece[0]) - 1
+    curr_col = ord(move_piece[1]) - ord('a')
+
+    new_row = int(move_to[0]) - 1
+    new_col = ord(move_to[1]) - ord('a')
+
+    return curr_row, curr_col, new_row, new_col
+
+
+# takes in string location and returns the index
+def parse_position(pos: str) -> (int, int):
+    row = int(pos[0]) - 1
+    col = ord(pos[1]) - ord('a')
+
+    return row, col
+
+
+# takes indexes and returns string input
+def un_parse_input(curr_row: int, curr_col: int, new_row: int, new_col: int):
+    move_piece = str(curr_row + 1) + chr(curr_col + 97)
+    move_to = str(new_row + 1) + chr(new_col + 97)
+
+    return move_piece + move_to
+
+
+def get_opposite_team(team):
+    if team == 'white':
+        return 'black'
+
+    if team == 'black':
+        return 'white'
